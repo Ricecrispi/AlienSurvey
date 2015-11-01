c6d10771bff215401535c9db613c11b4431c2dec
Christina <christina@artlocalapp.com>
2015-11-01 16:59:02 -0500

    Parse key

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,20 +3,10 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/aliensurvey-dev'
-  },
-  sequelize: {
-    uri: 'sqlite://',
-    options: {
-      logging: false,
-      storage: 'dev.sqlite',
-      define: {
-        timestamps: false
-      }
-    }
-  },
-
-  seedDB: true
+  PARSE_APPID: 'SwogwQVSRkqUYiIlIvLI577ONbX8aZbhXQCFo73X',
+  PARSE_JSKEY: 'gtO7QLHgfqOfJWQbqcUjmzc1Q4KtCpwoDdhtA4Hs',
+  //Sendwithus key?
+  SENDWITHUS: 'test_7e4531e75a0a9decee4f47feea2a19ae7e9119d2',
+  TEMPWD: 'temp123',
+  NOTIFICATION_EMAIL: ''
 };
