9fd9aa8b55ac69535912fcfc00cfcfd8b9a66f7f
Christina <christina@artlocalapp.com>
2015-11-01 12:35:29 -0500

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('alienSurveyApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
