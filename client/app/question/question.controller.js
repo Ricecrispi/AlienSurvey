5947a6892450307e1fb2fc17532f321e29a4892d
Christina <christina@artlocalapp.com>
2015-11-03 21:33:43 -0500

    Added question controller and HTML

--- client/app/question/question.controller.js
+++ client/app/question/question.controller.js
@@ -2,5 +2,35 @@
 
 angular.module('alienSurveyApp')
   .controller('QuestionCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    // array of alien images
+    $scope.questions = [];
+    // current question #
+    $scope.curQuestion = null;
+    // array of {x: , y:}
+    $scope.mouseClickLocations = [];
+
+    $scope.recordClick = function (event) {
+
+    };
+
+    $scope.saveAnswers = function () {
+      Restangular.all('api/participant/').post({answers: $scope.mouseClickLocations}).then(function (serverJson) {
+        $state.go('end');
+      });
+    };
+
+    $scope.getQuestions = function (answer) {
+      Restangular.all('api/question/').getList().then(function (serverJson) {
+        $state.questions = serverJson;
+      });
+    };
+
+    $scope.next = function () {
+      if ($scope.curQuestion < 5) {
+        $scope.curQuestion += 1;
+      } else {
+        $scope.saveAnswers();
+      }
+      ;
+    };
   });
